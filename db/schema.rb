1ccb2a551d49664082889a72b9bfae7aa91dc241
Brian <brianhuynh@uiowa.edu>
2025-11-28 23:40:16 -0600

    Committing changes for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -121,9 +121,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_28_223812) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "contacts", "users"
+  add_foreign_key "contacts", "users", column: "contact_user_id"
   add_foreign_key "discussion_messages", "discussions"
   add_foreign_key "discussion_messages", "users"
   add_foreign_key "discussions", "events"
+  add_foreign_key "event_attendees", "events"
+  add_foreign_key "event_attendees", "users"
   add_foreign_key "events", "users", column: "creator_id"
   add_foreign_key "gift_ideas", "recipients"
   add_foreign_key "gift_ideas", "users"
